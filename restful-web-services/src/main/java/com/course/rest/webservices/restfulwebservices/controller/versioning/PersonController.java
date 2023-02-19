ef1d9a28d2bddc73760bc3eef9418741d62b0d2b
nfebrian13 <nanaz.febrian@gmail.com>
2023-02-19 09:45:18 +0700

    rest versioning

--- restful-web-services/src/main/java/com/course/rest/webservices/restfulwebservices/controller/versioning/PersonController.java
+++ restful-web-services/src/main/java/com/course/rest/webservices/restfulwebservices/controller/versioning/PersonController.java
@@ -9,13 +9,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonController {
 
+    /*
+    * URI Versioning (Digunakan oleh Twitter)
+    * http://localhost:8080/v1/person
+    * http://localhost:8080/v2/person
+    *
+    * */
+
     @GetMapping("/v1/person")
     public PersonV1 getFirstVersionOfPerson() {
         return new PersonV1("Bob Charlie");
     }
 
     @GetMapping("/v2/person")
-    public PersonV2 getFirstVersion2OfPerson() {
+    public PersonV2 getSecondVersionOfPerson() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    /*
+     * URI Versioning (Digunakan oleh Amazon)
+     * http://localhost:8080/version=1
+     * http://localhost:8080/version=2
+     *
+     * */
+
+    @GetMapping(path ="/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonReqParam() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path ="/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonReqParam() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
 }

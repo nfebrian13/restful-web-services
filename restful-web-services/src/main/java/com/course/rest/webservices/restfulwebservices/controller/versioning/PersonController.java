61d0aee93f4461c4d3c2bde75f88d0d777010a95
nfebrian13 <nanaz.febrian@gmail.com>
2023-02-19 09:37:06 +0700

    rest versioning

--- restful-web-services/src/main/java/com/course/rest/webservices/restfulwebservices/controller/versioning/PersonController.java
+++ restful-web-services/src/main/java/com/course/rest/webservices/restfulwebservices/controller/versioning/PersonController.java
@@ -1,6 +1,8 @@
 package com.course.rest.webservices.restfulwebservices.controller.versioning;
 
+import com.course.rest.webservices.restfulwebservices.model.versioning.Name;
 import com.course.rest.webservices.restfulwebservices.model.versioning.PersonV1;
+import com.course.rest.webservices.restfulwebservices.model.versioning.PersonV2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonController {
 
     @GetMapping("/v1/person")
-    public PersonV1 getFirstVersionOfPerson(){
+    public PersonV1 getFirstVersionOfPerson() {
         return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping("/v2/person")
+    public PersonV2 getFirstVersion2OfPerson() {
+        return new PersonV2(new Name("Bob", "Charlie"));
     }
 }

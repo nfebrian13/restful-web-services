f54171013320b387335dc444b52d418ab1100de2
nfebrian13 <nanaz.febrian@gmail.com>
2023-02-19 09:07:55 +0700

    added multiple languages message properties

--- restful-web-services/src/main/java/com/course/rest/webservices/restfulwebservices/controller/HelloWorldController.java
+++ restful-web-services/src/main/java/com/course/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.course.rest.webservices.restfulwebservices.controller;
 
 import com.course.rest.webservices.restfulwebservices.model.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/hello-world")
     public String helloWorld() {
@@ -22,6 +30,12 @@ public class HelloWorldController {
     @GetMapping("/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean("Hello World, " + name);
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 
 }

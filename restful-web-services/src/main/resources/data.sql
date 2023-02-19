70ee56380b6760b4748c425796ce1b251ae29208
nfebrian13 <nanaz.febrian@gmail.com>
2023-02-19 18:57:50 +0700

    added jpa repository

--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -6,3 +6,15 @@ values(1002, current_date(), 'Nana');
 
 insert into user_details(id,birth_date, name)
 values(1003, current_date(), 'Tari');
+
+insert into post(id,description, user_id)
+values(2001, 'i want to learn AWS', 1001);
+
+insert into post(id,description, user_id)
+values(2002, 'i want to learn AWS', 1001);
+
+insert into post(id,description, user_id)
+values(2003, 'i want to learn AWS Certified', 1002);
+
+insert into post(id,description, user_id)
+values(2004, 'i want to learn Multi Cloud', 1002);
